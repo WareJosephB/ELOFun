737cd2cf8451493581d425398f064381bd5f3049
WareJosephB <ware.josephb@gmail.com>
2018-11-08 18:48:55 +0000

    End of day, 8/11/18

--- src/main/java/runner/Iterator.java
+++ src/main/java/runner/Iterator.java
@@ -12,64 +12,87 @@ public class Iterator {
 
 	public static void main(String[] args) {
 
+		Player alvin = new Player("Alvin");
 		Player andy = new Player("Andy");
 		Player ben = new Player("Ben");
 		Player gareth = new Player("Gareth");
 		Player harrison = new Player("Harrison");
 		Player joseph = new Player("Joseph");
 		Player lewis = new Player("Lewis");
+		Player lysien = new Player("Lysien");
 		Player naveed = new Player("Naveed");
+		Player mahamed = new Player("Mahamed");
+		Player dev = new Player("Dev");
+		Player matt = new Player("Matt");
+		Player vinul = new Player("Vinul");
+		Player daud = new Player("Daud");
+		Player malcolm = new Player("Malcolm");
 
-		for (int i = 0; i < 10; i++) {
-		ELOCalc.Victory(andy, gareth);
-		ELOCalc.Victory(joseph, gareth);
-		ELOCalc.Victory(ben, andy);
-		ELOCalc.Victory(ben, harrison);
-		// End of day 30th October
+		for (int i = 0; i < 30; i++) {
+			ELOCalc.Victory(harrison, alvin);
+			ELOCalc.Victory(andy, gareth);
+			ELOCalc.Victory(joseph, gareth);
+			ELOCalc.Victory(ben, andy);
+			ELOCalc.Victory(ben, harrison);
+			// End of day 30th October
 
-		ELOCalc.Victory(gareth, andy);
-		ELOCalc.Victory(harrison, joseph);
-		ELOCalc.Victory(ben, joseph);
-		ELOCalc.Victory(harrison, andy);
-		ELOCalc.Victory(naveed, lewis);
-		
-		// End of day, 31st October
+			ELOCalc.Victory(gareth, andy);
+			ELOCalc.Victory(harrison, joseph);
+			ELOCalc.Victory(naveed, andy);
+			ELOCalc.Victory(ben, joseph);
+			ELOCalc.Victory(harrison, andy);
+			ELOCalc.Victory(naveed, lewis);
+			ELOCalc.Victory(naveed, lysien);
+			ELOCalc.Victory(lysien, lewis);
+			// End of day, 31st October
 
-		ELOCalc.Victory(andy, gareth);
-		ELOCalc.Victory(ben, gareth);
-		ELOCalc.Victory(lewis, joseph);
-		// End of day, 1st November
+			ELOCalc.Victory(andy, gareth);
+			ELOCalc.Victory(harrison, alvin);
+			ELOCalc.Victory(ben, gareth);
+			ELOCalc.Victory(lewis, joseph);
+			// End of day, 1st November
 
-		ELOCalc.Victory(andy, gareth);
-		ELOCalc.Victory(andy, joseph);
-		ELOCalc.Victory(joseph, gareth);
-		ELOCalc.Victory(ben, harrison);
-		ELOCalc.Victory(harrison, andy);
-		ELOCalc.Victory(ben, harrison);
-		// End of day, 2nd November
+			ELOCalc.Victory(andy, gareth);
+			ELOCalc.Victory(andy, joseph);
+			ELOCalc.Victory(joseph, gareth);
+			ELOCalc.Victory(ben, harrison);
+			ELOCalc.Victory(harrison, andy);
+			ELOCalc.Victory(ben, harrison);
+			// End of day, 2nd November
 
-		ELOCalc.Victory(andy, lewis);
-		ELOCalc.Victory(gareth, joseph);
-		ELOCalc.Victory(ben, joseph);
-		ELOCalc.Victory(ben, andy);
-		ELOCalc.Victory(harrison, naveed);
-		ELOCalc.Victory(andy, joseph);
-		ELOCalc.Victory(harrison, naveed);
-		// End of day, 5th November
-		
-		ELOCalc.Victory(andy, gareth);
-		ELOCalc.Victory(ben, joseph);
-		ELOCalc.Victory(joseph, naveed);
-		//End of day, 6th November
-		
-		
-		ELOCalc.Victory(joseph, gareth);}
+			ELOCalc.Victory(mahamed, harrison);
+			ELOCalc.Victory(andy, lewis);
+			ELOCalc.Victory(gareth, joseph);
+			ELOCalc.Victory(alvin, harrison);
+			ELOCalc.Victory(lysien, daud, true);
+			ELOCalc.Victory(ben, joseph);
+			ELOCalc.Victory(ben, andy);
+			ELOCalc.Victory(harrison, naveed);
+			ELOCalc.Victory(andy, joseph);
+			ELOCalc.Victory(harrison, naveed);
+			ELOCalc.Victory(matt, andy);
+			ELOCalc.Victory(dev, joseph);
+			ELOCalc.Victory(harrison, vinul);
+			// End of day, 5th November
+			
+			ELOCalc.Victory(andy, gareth);
+			ELOCalc.Victory(ben, joseph);
+			ELOCalc.Victory(joseph, malcolm);
+			ELOCalc.Victory(naveed, daud);
+			ELOCalc.Victory(joseph, naveed);
+			//End of day, 6th November
+			
+			ELOCalc.Victory(alvin, harrison);
+			ELOCalc.Victory(lewis, malcolm);
+			ELOCalc.Victory(joseph, gareth);
+			ELOCalc.Victory(ben, dev);
+			ELOCalc.Victory(gareth, malcolm);}
 		
 		//End of day, 7th November
 
 		
 		
-		List<Player> playerList =Arrays.asList(andy, ben, gareth, harrison, joseph, lewis, naveed);
+		List<Player> playerList =Arrays.asList(alvin, andy, ben, gareth, harrison, joseph, lewis, lysien, naveed, dev, vinul, daud, matt, mahamed, malcolm);
 		Collections.sort(playerList, Comparator.comparing(i -> -i.getELO()));
 		for (Player player : playerList) {
 			System.out.println(player);
--- src/main/java/runner/runner.java
+++ src/main/java/runner/runner.java
@@ -86,11 +86,15 @@ public class runner {
 		ELOCalc.Victory(joseph, gareth);
 		ELOCalc.Victory(ben, dev);
 		//End of day, 7th November
-
+		
 		ELOCalc.Victory(andy, joseph);
 		ELOCalc.Victory(ben, gareth);
 		ELOCalc.Victory(alvin, harrison);
 		ELOCalc.Victory(andy, ben);
+		ELOCalc.Victory(malcolm, joseph);
+		ELOCalc.Victory(andy, lewis);
+		ELOCalc.Victory(joseph, andy);
+		//End of day, 8th November
 		
 		List<Player> playerList =Arrays.asList(alvin, andy, ben, gareth, harrison, joseph, lewis, lysien, naveed, dev, vinul, daud, matt, mahamed, malcolm);
 		Collections.sort(playerList, Comparator.comparing(i -> -i.getELO()));
